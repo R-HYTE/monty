040d533f3f44b966ac674030ddd085aa92177206
R_HYTE <polycarpwathuta39@gmail.com>
2023-10-22 14:57:22 +0300

    sets the format of the data to a queue (FIFO)

---
+++ queue.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * _queue - Flag allocation for queues case
+ * @stack: pointer to the pointer to the head of the stack
+ * @line_number: current line number in the file being read
+ * Return: Void
+ */
+void _queue(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+	pub.ads_flag = 1;
+}
