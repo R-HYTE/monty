5ea34b8963d4059463b13a917446239df7cc17f0
R_HYTE <polycarpwathuta39@gmail.com>
2023-10-22 14:56:59 +0300

    sets the format of the data to a stack (LIFO)

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * _stack - Flag allocation in case of a stack
+ * @stack: pointer to the pointer to the list
+ * @line_number: current line number in the file being read
+ * Return: Void
+ */
+void _stack(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+	pub.ads_flag = 0;
+}
