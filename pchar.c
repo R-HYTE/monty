ce7f3fe6c9244574ebceb882e58cca62da37a96d
R_HYTE <polycarpwathuta39@gmail.com>
2023-10-22 14:55:25 +0300

    prints the char at the top of the stack,

---
+++ pchar.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+
+/**
+ * _pchar - Prints the char at the top of the stack
+ * @stack: pointer to the pointer to the list
+ * @line_number: Current line number in the file being read
+ * Return: Void
+ */
+void _pchar(stack_t **stack, unsigned int line_number)
+{
+	stack_t *curr;
+
+	curr = *stack;
+	/* When stack is empty*/
+	if (curr == NULL)
+	{
+		fprintf(stderr, "L%d: Can't pchar, stack empty\n", line_number);
+		fclose(pub.file);
+		free(pub.string);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	/*Outside ASCII range*/
+	if (curr->n > 127 || curr->n < 0)
+	{
+		fprintf(stderr, "L%d: Can't pchar, value out of range\n", line_number);
+		fclose(pub.file);
+		free(pub.string);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", curr->n);
+}
